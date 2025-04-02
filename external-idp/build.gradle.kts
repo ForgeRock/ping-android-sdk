82f1630c381c5662a356f1fcc89e6a36429ab1df
Stoyan Petrov <stoyan@petrov.ca>
2025-04-02 22:28:56 +0300

    SDKS-3878 Release activities for Ping SDK 1.1.0 (#42)
    * Updated CHANGELOG.md for the 1.1.0 release.
    * SDKS-3907 Update copyright header to reference Ping Identity and current year

--- external-idp/build.gradle.kts
+++ external-idp/build.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 - 2025 Ping Identity. All rights reserved.
+ * Copyright (c) 2024 - 2025 Ping Identity Corporation. All rights reserved.
  *
  * This software may be modified and distributed under the terms
  * of the MIT license. See the LICENSE file for details.
@@ -7,6 +7,8 @@
 
 plugins {
     id("com.pingidentity.convention.android.library")
+    id("com.pingidentity.convention.centralPublish")
+    id("com.pingidentity.convention.jacoco")
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.kotlinAndroid)
 }
--- samples/journeyapp/src/main/java/com/pingidentity/samples/journeyapp/journey/callback/SelectIdpCallback.kt
+++ samples/journeyapp/src/main/java/com/pingidentity/samples/journeyapp/journey/callback/SelectIdpCallback.kt
@@ -1,9 +1,10 @@
 /*
- * Copyright (c) 2024 PingIdentity. All rights reserved.
+ * Copyright (c) 2024 - 2025 Ping Identity Corporation. All rights reserved.
  *
  * This software may be modified and distributed under the terms
  * of the MIT license. See the LICENSE file for details.
  */
+
 
 package com.pingidentity.samples.journeyapp.journey.callback
 
