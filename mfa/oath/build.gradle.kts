8eea1ab25cd7526438ddfbb2b4ebdad349cb1aad
Stoyan Petrov <stoyan@petrov.ca>
2025-09-24 15:41:21 -0700

    Commented out the publish part. We will release the OATH and PUSH modules in version 2.0

--- mfa/oath/build.gradle.kts
+++ mfa/oath/build.gradle.kts
@@ -7,7 +7,7 @@
 
 plugins {
     id("com.pingidentity.convention.android.library")
-    id("com.pingidentity.convention.centralPublish")
+//    id("com.pingidentity.convention.centralPublish") // we are not publishing this module for now
     id("com.pingidentity.convention.jacoco")
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.kotlinAndroid)
