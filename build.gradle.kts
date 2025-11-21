cab2ca4f493b8f37b7faaad407619125ab236e5f
vibhorgoswami <vibhor.goswami@gmail.com>
2025-11-21 12:08:07 -0800

    Fix: Address security vulnerability in `play-services-basement`
    This commit forces the version of `com.google.android.gms:play-services-basement` to `18.0.2`.
    
    This is a transitive dependency of `recaptcha:recaptcha`, and the update addresses security vulnerability CVE-2022-2390.

--- build.gradle.kts
+++ build.gradle.kts
@@ -62,6 +62,11 @@ allprojects {
             force("com.google.protobuf:protobuf-kotlin:4.29.2")
             force("com.google.protobuf:protobuf-javalite:4.29.2")
             force("com.google.protobuf:protobuf-kotlin-lite:4.29.2")
+
+            // Force secure version of play-services-basement to address security vulnerabilities
+            // Used transitively by recaptcha:recaptcha
+            // Updated to 18.0.2 which fixes CVE-2022-2390
+            force("com.google.android.gms:play-services-basement:18.0.2")
         }
     }
 }
