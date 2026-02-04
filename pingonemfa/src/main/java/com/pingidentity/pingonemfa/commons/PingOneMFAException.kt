75a3ad2cf5bde80fb7a04b448bcc7a43cabb55f2
Evgeniy Mishustin <evgeniymishustin@pingidentity.com>
2026-02-04 09:49:50 +0200

    Added copyright header to each module file

--- pingonemfa/src/main/java/com/pingidentity/pingonemfa/commons/PingOneMFAException.kt
+++ pingonemfa/src/main/java/com/pingidentity/pingonemfa/commons/PingOneMFAException.kt
@@ -1,6 +1,12 @@
-package com.pingidentity.pingonemfa.commons
 /*
  * Copyright (c) 2026 Ping Identity Corporation. All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license. See the LICENSE file for details.
+ */
+
+package com.pingidentity.pingonemfa.commons
+/*
  * Wrapper for error message into exception class
  */
 class PingOneMFAException(message: String?) : Exception(message)
