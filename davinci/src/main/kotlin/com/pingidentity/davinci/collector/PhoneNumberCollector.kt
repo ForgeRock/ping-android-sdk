6103338324ed55d3a055d93a7fde17dc615b2147
Vibhor Goswami <vibhor.goswami@gmail.com>
2026-04-15 10:00:06 -0700

    SDKS-4669: Add support for phone number extensions in PhoneNumberCollector (#177)

--- davinci/src/main/kotlin/com/pingidentity/davinci/collector/PhoneNumberCollector.kt
+++ davinci/src/main/kotlin/com/pingidentity/davinci/collector/PhoneNumberCollector.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025 Ping Identity Corporation. All rights reserved.
+ * Copyright (c) 2025 - 2026 Ping Identity Corporation. All rights reserved.
  *
  * This software may be modified and distributed under the terms
  * of the MIT license. See the LICENSE file for details.
@@ -16,9 +16,12 @@ import kotlinx.serialization.json.put
 
 private const val COUNTRY_CODE = "countryCode"
 private const val PHONE_NUMBER = "phoneNumber"
+private const val EXTENSION = "extension"
 
 private const val DEFAULT_COUNTRY_CODE = "defaultCountryCode"
 private const val VALIDATE_PHONE_NUMBER = "validatePhoneNumber"
+private const val SHOW_EXTENSION = "showExtension"
+private const val EXTENSION_LABEL = "extensionLabel"
 
 /**
  * A collector for phone number.
@@ -31,16 +34,24 @@ class PhoneNumberCollector : FieldCollector<JsonObject>(), Validator {
 
     var validatePhoneNumber: Boolean = false
         private set
+    var showExtension: Boolean = false
+        private set
+    var extensionLabel: String = ""
+        private set
 
     // country code
     var countryCode: String = ""
     // phone number
     var phoneNumber: String = ""
+    // extension
+    var extension: String = ""
 
     override fun init(input: JsonObject) : PhoneNumberCollector {
         super.init(input)
         defaultCountryCode = input[DEFAULT_COUNTRY_CODE]?.jsonPrimitive?.content ?: ""
         validatePhoneNumber = input[VALIDATE_PHONE_NUMBER]?.jsonPrimitive?.boolean ?: false
+        showExtension = input[SHOW_EXTENSION]?.jsonPrimitive?.boolean ?: false
+        extensionLabel = input[EXTENSION_LABEL]?.jsonPrimitive?.content ?: ""
         return this
     }
 
@@ -49,6 +60,8 @@ class PhoneNumberCollector : FieldCollector<JsonObject>(), Validator {
             // New structure with phoneNumber and countryCode
             phoneNumber = input[PHONE_NUMBER]?.jsonPrimitive?.content ?: ""
             countryCode = input[COUNTRY_CODE]?.jsonPrimitive?.content ?: ""
+            extension = input[EXTENSION]?.jsonPrimitive?.content ?: ""
+            extensionLabel = input[EXTENSION_LABEL]?.jsonPrimitive?.content ?: ""
         } else {
             // Legacy structure - simple string
             phoneNumber = input.jsonPrimitive.content
@@ -62,6 +75,7 @@ class PhoneNumberCollector : FieldCollector<JsonObject>(), Validator {
             buildJsonObject {
                 put(COUNTRY_CODE, countryCode)
                 put(PHONE_NUMBER, phoneNumber)
+                put(EXTENSION, extension)
             }
         }
     }
