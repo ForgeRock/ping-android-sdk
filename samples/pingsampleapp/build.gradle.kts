9a26d2ee30c136f99de775bb6a4e98486c009471
vibhorgoswami <vibhor.goswami@gmail.com>
2026-06-30 13:24:39 -0700

    SDKS-5191 Update SDK versions in build configuration

--- samples/pingsampleapp/build.gradle.kts
+++ samples/pingsampleapp/build.gradle.kts
@@ -9,12 +9,12 @@ plugins {
 
 android {
     namespace = "com.pingidentity.samples.pingsampleapp"
-    compileSdk = 36
+    compileSdk = libs.versions.compileSdk.get().toInt()
 
     defaultConfig {
         applicationId = "com.pingidentity.samples.pingsampleapp"
-        minSdk = 29
-        targetSdk = 36
+        minSdk = libs.versions.minSdk.get().toInt()
+        targetSdk = libs.versions.targetSdk.get().toInt()
         versionCode = 1
         versionName = "1.0"
 
