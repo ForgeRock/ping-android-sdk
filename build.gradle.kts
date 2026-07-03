29ac24fd70b31ba0ac40be822a88b975469840f6
vibhorgoswami <vibhor.goswami@gmail.com>
2026-07-03 10:01:12 -0700

    Fix security issues by forcing correct versions.

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,17 +38,23 @@ nexusPublishing {
     }
 }
 
-allprojects {
+buildscript {
     configurations.all {
-
         resolutionStrategy {
+            // Force secure version of bouncy castle to address security vulnerabilities
+            // Fixes CVE-2025-14813 and CVE-2026-5598; targets build-toolchain classpath (lint-gradle)
+            force("org.bouncycastle:bcprov-jdk18on:1.84")
+            force("org.bouncycastle:bcpkix-jdk18on:1.84")
+            force("org.bouncycastle:bcutil-jdk18on:1.84")
+
             // Due to vulnerability [WS-2022-0468] from dokka project.
+            // Targets build-toolchain classpath (Dokka/AGP); jackson is not a subproject dep.
             force("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.0")
             force("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:2.15.0")
             force("com.fasterxml.jackson.core:jackson-databind:2.15.0")
 
             // Force secure version of netty-codec to address security vulnerabilities
-            // Used transitively by com.android.tools.emulator:proto
+            // Used transitively by com.android.tools.emulator:proto (build toolchain only)
             // Updated to 4.1.125.Final per Mend SCA recommendation (Nov 2025)
             force("io.netty:netty-codec:4.1.125.Final")
             force("io.netty:netty-codec-http:4.1.125.Final")
@@ -56,19 +62,27 @@ allprojects {
             force("io.netty:netty-all:4.1.125.Final")
 
             // Force secure version of protobuf to address security vulnerabilities
-            // Used transitively by various Google dependencies
+            // Used transitively by AGP/build toolchain
             // Updated to 4.29.2 (latest stable) which fixes CVE-2024-7254 and all known CVEs
             force("com.google.protobuf:protobuf-java:4.29.2")
             force("com.google.protobuf:protobuf-kotlin:4.29.2")
             force("com.google.protobuf:protobuf-javalite:4.29.2")
             force("com.google.protobuf:protobuf-kotlin-lite:4.29.2")
+        }
+    }
+}
 
+allprojects {
+    configurations.all {
+
+        resolutionStrategy {
             // Force secure version of play-services-basement to address security vulnerabilities
-            // Used transitively by recaptcha client
+            // Used transitively by recaptcha client and fido subprojects
             // Updated to 18.0.2 which fixes CVE-2022-2390
             force("com.google.android.gms:play-services-basement:18.0.2")
+
             // Force secure version of nimbus-jose-jwt to address security vulnerabilities
-            // Force the existing to fix CVE-2025-53864
+            // Used transitively by mfa:binding subproject; fixes CVE-2025-53864
             force("com.nimbusds:nimbus-jose-jwt:10.5")
         }
     }
