c9fae6c8c338b3219a50e657c6fb955d3bbd03b3
Vibhor Goswami <vibhor.goswami@gmail.com>
2026-08-10 15:59:15 -0700

    SDKS-5260: Enhance policies parsing in AbstractValidatedCallback (#235)

--- journey/src/main/kotlin/com/pingidentity/journey/callback/AbstractValidatedCallback.kt
+++ journey/src/main/kotlin/com/pingidentity/journey/callback/AbstractValidatedCallback.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 - 2025 Ping Identity Corporation. All rights reserved.
+ * Copyright (c) 2024 - 2026 Ping Identity Corporation. All rights reserved.
  *
  * This software may be modified and distributed under the terms
  * of the MIT license. See the LICENSE file for details.
@@ -58,7 +58,7 @@ abstract class AbstractValidatedCallback : AbstractCallback() {
 
     override fun init(name: String, value: JsonElement) {
         when (name) {
-            "policies" -> policies = value.jsonObject
+            "policies" -> policies = (value as? JsonObject) ?: buildJsonObject {}
             "failedPolicies" -> parseFailedPolicy(value.jsonArray)
             "validateOnly" -> validateOnly = value.jsonPrimitive.boolean
             "prompt" -> prompt = value.jsonPrimitive.content
